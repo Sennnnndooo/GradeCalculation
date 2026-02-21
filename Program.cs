f34186cfd93cc6b20f03f901548101032cabc173
MALLON <uchihamadara4289@gmail.com>
2026-02-21 17:24:23 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GradeCalculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Grade Computation ");
+            Console.WriteLine("Student Name:");
+            string studentname = Console.ReadLine();
+            Console.WriteLine("Year & Course:");
+            var yearandcourse = Console.ReadLine(); 
+
+
+
+        }
+    }
+}
