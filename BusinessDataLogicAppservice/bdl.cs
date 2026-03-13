5658be60e634337f06e3e1107e7d6479e91f573c
Sennddooo <uchihamadara4289@gmail.com>
2026-03-13 23:22:12 +0800

    Grade Calculation(2)

---
+++ BusinessDataLogicAppservice/bdl.cs
@@ -0,0 +1,39 @@
+﻿using DataLogicDataService;
+namespace BusinessDataLogicAppservice
+{
+    public class bdl
+    {
+        dl data = new dl();
+
+        public class GradeResult
+        {
+            public double FinalQuiz1, FinalQuiz2, FinalQuiz3;
+            public double FinalLongQuiz1, FinalLongQuiz2;
+            public double FinalProject, FinalPerform, FinalMid, FinalFinals;
+            public double TotalGrade;
+        }
+
+        public GradeResult ComputeGrade(double QUIZ1, double QUIZ2, double QUIZ3, double LNGQUIZ1, double LNGQUIZ2, double PROJECT, double PERFORM, double MIDTERMS, double FINALS)
+        {
+            GradeResult result = new GradeResult();
+
+            result.FinalQuiz1 = QUIZ1 / data.quizscore1;
+            result.FinalQuiz2 = QUIZ2 / data.quizscore2;
+            result.FinalQuiz3 = QUIZ3 / data.quizscore3;
+
+            result.FinalLongQuiz1 = LNGQUIZ1 / data.lngquizscore1;
+            result.FinalLongQuiz2 = LNGQUIZ2 / data.lngquizscore2;
+
+            result.FinalProject = PROJECT / data.project1;
+            result.FinalPerform = PERFORM / data.perform1;
+            result.FinalMid = MIDTERMS / data.midterms1;
+            result.FinalFinals = FINALS / data.finals1;
+
+            double rawscore = (data.quizscore1 * result.FinalQuiz1) + (data.quizscore2 * result.FinalQuiz2) + (data.quizscore3 * result.FinalQuiz3) + (data.lngquizscore1 * result.FinalLongQuiz1) + (data.lngquizscore2 * result.FinalLongQuiz2) + (data.project1 * result.FinalProject) + (data.perform1 * result.FinalPerform) + (data.midterms1 * result.FinalMid) + (data.finals1 * result.FinalFinals);
+
+            result.TotalGrade = (rawscore / data.totalscore) * data.hundredpercent;
+
+            return result;
+        }
+    }
+}
---
+++ DataLogicDataService/dl.cs
@@ -0,0 +1,21 @@
+﻿namespace DataLogicDataService
+{
+    public class dl
+    {
+
+        public double quizscore1 = 10;
+        public double quizscore2 = 10;
+        public double quizscore3 = 10;
+
+        public double lngquizscore1 = 20;
+        public double lngquizscore2 = 30;
+
+        public double project1 = 50;
+        public double perform1 = 50;
+        public double midterms1 = 50;
+        public double finals1 = 50;
+
+        public double totalscore = 280;
+        public double hundredpercent = 100;
+    }
+}
