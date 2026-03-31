040f3eb7be9a633e82b20ab78ac4292c7f1a7fad
Sennnnndooo <uchihamadara4289@gmail.com>
2026-04-01 02:59:17 +0800

    Update GradeCalculationInitiator.cs

--- GradeCalculationDataLogicLayer/GradeCalculationInitiator.cs
+++ GradeCalculationDataLogicLayer/GradeCalculationInitiator.cs
@@ -1,30 +1,14 @@
 ﻿using GradeCalculationDataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace GradeCalculationDataLogicLayer
 {
     public class GradeCalculationInitiator
     {
-        private readonly GradeCalculationIni data;
-
-        public GradeCalculationInitiator(GradeCalculationIni DATA)
-        {
-            data = DATA;
-        }
-        public void Add(StudentGrade grade)
-        {
-            data.Add(grade);
-        }
-        public List<StudentGrade> GetInfo()
-        {
-            return data.GetInfo();
-        }
-        public void Update(StudentGrade grade)
-        {
-            data.Update(grade);
-        }
-        public void Delete(string name)
-        {   
-            data.Delete(name);
-        }
+      
     }
 }
